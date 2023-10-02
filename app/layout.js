b6b8f0063d01bae152b9d2f3807cc5e6a9e0c37c
Marie Starck <marie.compsci14@gmail.com>
2023-10-02 18:59:34 -0400

    Updated project Next.js + MUI

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import ThemeRegistry from '@/utils/ThemeRegistry'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,7 +12,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ThemeRegistry options={{ key: 'mui' }}>{children}</ThemeRegistry>
+      </body>
     </html>
   )
 }
---
+++ utils/theme.js
@@ -0,0 +1,11 @@
+import { createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+ palette: {
+   primary: {
+     main: "#fcba03",
+   },
+ },
+});
+
+export default theme;
